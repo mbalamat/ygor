3725dae4479ff95ab0f54d405b7bc305d549809f
mbalamat <mbalamatsias@gmail.com>
2017-03-04 16:32:06 +0200

    Fix notify and mail format

--- notify.py
+++ notify.py
@@ -6,12 +6,21 @@ try:
 except:
     print 'Env variables not set please set EMAIL_ADDRESS and EMAIL_PASSWD.'
 
-def notify(notification_method, receiver_address, message):
+def notify(notification_method, receiver_address, url):
+    html_mail = '''\
+    Alert! change on CHANGETHIS.
+
+    Hi there was a change on one of your monitored sites, please follow the link bellow:
+
+    CHANGETHIS
+    '''
+    html_mail = html_mail.replace('CHANGETHIS', url)
     if notification_method == 'email':
         server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
         server.starttls()
         server.login(email_address, email_passwd)
-        server.sendmail(email_address, receiver_address, message)
+        server.sendmail(email_address, receiver_address, html_mail)
         server.quit()
     else:
-        print 'Local Notification: ' + message
+        print 'Local Notification: \n' + html_mail
